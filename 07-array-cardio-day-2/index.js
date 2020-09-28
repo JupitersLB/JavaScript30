b055a390d60d919832c7243825d6050a5396995f
Liam Baker <liam.baker2310@gmail.com>
2020-09-28 13:01:24 +0900

    finished last challenge

--- 07-array-cardio-day-2/index.js
+++ 07-array-cardio-day-2/index.js
@@ -35,3 +35,10 @@
     // Array.prototype.findIndex()
     // Find the comment with this ID
     // delete the comment with the ID of 823423
+
+    const index = comments.findIndex(comment => comment.id === 823423);
+
+    const newComments = [
+      ...comments.slice(0, index),
+      ...comments.slice(index + 1)
+    ];
