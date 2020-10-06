79c095491a9044afc644d0210af77aed97896e83
Liam Baker <liam.baker2310@gmail.com>
2020-10-06 10:13:27 +0900

    finished challege

--- 09-14-must-know-dev-tools-tricks/index.js
+++ 09-14-must-know-dev-tools-tricks/index.js
@@ -7,25 +7,40 @@ function makeGreen() {
 }
 
 // Regular
-
+console.log('hello');
 // Interpolated
-
+console.log('Hello I am a %s string!', '💩' );
 // Styled
-
+console.log('%c I am some great text', 'font-size: 30px;');
 // warning!
-
+console.warn('OH NOOO');
 // Error :|
-
+console.error('Shit!');
 // Info
-
+console.info('Crocodiles eat 3-4 people per year');
 // Testing
-
+const p = document.querySelector('p');
+console.assert(p.classList.contains('ouch'), 'That is wrong!');
 // clearing
-
+console.clear();
 // Viewing DOM Elements
-
+console.dir(p);
 // Grouping together
-
+dogs.forEach(dog => {
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`This is ${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.groupEnd(`${dog.name}`);
+})
 // counting
-
+console.count('Wes')
+console.count('Wes')
+console.count('Wes')
 // timing
+console.time('fetching data');
+fetch('https://api.github.com/users/jupiterslb')
+  .then(data => data.json())
+  .then(data => {
+    console.timeEnd('fetching data');
+    console.log(data);
+});
